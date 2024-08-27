a3f12fbe88142c484d9f4ed1d0c227810c1eff75
Prakhar Srivastav <prakhar.srivastav@finarkein.com>
2024-08-27 12:32:47 +0530

    Test Param

--- demo.py
+++ demo.py
@@ -1,28 +1,10 @@
-from datetime import datetime
-import time
-import requests
-import requests.auth
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import get_current_context
 from airflow.models.param import Param
 
-
-url='http://localhost:8000/api/v1/connections/list'
-
-post_data={"workspaceId":"f97ed1d1-3d06-4612-92c2-df50b0386e0c"}
-
-username="airbyte"
-password="password"
-
-# response=requests.post(url,json=post_data,auth=requests.auth.HTTPBasicAuth(username,password)).json()
-
 commands=["echo airflow", "echo finarkein", "echo test", "echo development", "echo DAG", "echo kubernetes", "echo minikube"]
-
-# for connection in response['connections']:
-#     commands.append("echo hello {}".format(connection['name']))
-
 
 workers=3
 
@@ -31,57 +13,16 @@ with DAG(dag_id="demo_DAG", params={"commands":Param(commands,type='array',title
  ) as dag:
     # Tasks are represented as operators
 
+    @task
+    def get_commands():
+        context = get_current_context()
+        return context["params"]["commands"]
+
     hello = BashOperator(task_id="hello", bash_command="echo hello world")
 
-    tasks=BashOperator.partial(task_id='test',max_active_tis_per_dag=workers,map_index_template='{{task.bash_command}}').expand(bash_command=commands)
-    
-    @task()
-    def airflow1():
-        time.sleep(5)
-        print("airflow1 is working")
-
-    @task()
-    def airflow2():
-        time.sleep(5)
-        print("airflow2 is working")
-    
-    @task()
-    def airflow3():
-        time.sleep(5)
-        print("airflow3 is working")
-    
-    @task()
-    def airflow4():
-        time.sleep(12)
-        print("airflow4 is working")
-    
-    @task()
-    def airflow5():
-        time.sleep(15)
-        print("airflow5 is working")
-    
-    @task()
-    def airflow6():
-        time.sleep(3)
-        print("airflow6 is working")
-    
-    @task()
-    def airflow7():
-        time.sleep(10)
-        print("airflow7 is working")
-
-    @task()
-    def airflow8():
-        time.sleep(5)
-        print("airflow8 is working")
-
-    @task()
-    def airflow9():
-        time.sleep(8)
-        print("airflow9 is working")
+    tasks=BashOperator.partial(task_id='test',max_active_tis_per_dag=workers,map_index_template='{{task.bash_command}}').expand(bash_command=get_commands())
 
     bye = BashOperator(task_id="bye", bash_command="echo bye world")
 
     # Set dependencies between tasks
-    # hello >> airflow1() >> airflow2() >> [airflow3(), airflow4(), airflow5(), airflow6(), airflow7(), airflow8(), airflow9()] >> bye
     hello>>tasks>>bye
