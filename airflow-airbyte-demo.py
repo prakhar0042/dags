a64416efdf9c0ef2211aead9c980e4e78f50da58
Prakhar Srivastav <prakhar.srivastav@finarkein.com>
2024-08-28 12:54:38 +0530

    Removing hardcoded connections

--- airflow-airbyte-demo.py
+++ airflow-airbyte-demo.py
@@ -17,13 +17,7 @@ response = requests.post(
     url, json = post_data, auth = requests.auth.HTTPBasicAuth(username, password)
 ).json()
 
-connections = {"d42ebce0-46ee-447b-bdd9-e3b30688cf0b":"Demo 6",
-               "d7f499f1-315d-4f6b-8137-7be948c6ab3f":"Demo 1",
-               "6f32576f-4806-4a87-8518-75d7957dff5b":"Demo 3",
-               "f3a692a0-a989-4f3c-b143-2257f46782cb":"Demo 5",
-               "1e2f32aa-1214-4dd1-9646-f8c5e58b905d":"Demo 2",
-               "63a0f92d-6623-40cf-901f-e0160cedad0c":"Demo 7",
-               "bb746c31-1b2d-4541-a4e6-6f413d3a87f9":"Demo 4"}
+connections = {}
 
 for connection in response["connections"]:
     connections[connection['connectionId']] = connection['name']
